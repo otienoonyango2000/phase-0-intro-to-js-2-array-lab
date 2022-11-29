a60fd044f53977fdf605bc74641f7bc2f455c4f3
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-11-29 13:35:41 +0300

    js 2 array lab completed

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield",];
+let append = [...cats, "Broom"];
+let forward = ["Arnold", ...cats];
+let lastWard = ['Milo', 'Otis', 'Garfield' ]
+
+
+function destructivelyAppendCat(name){
+  return cats.push("Ralph");
+
+}
+
+function destructivelyPrependCat(name){
+  return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+  return cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(){
+  return cats.shift("Milo");
+}
+
+function appendCat(name){
+  
+  return append.slice("Broom");
+}
+
+function prependCat(name){
+  return forward.slice("Arnold");
+}
+
+function removeLastCat(){
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+  return cats.slice(1);
+}
+
+
+
+
+  
